d0fccdfc8cbccd1475c33bf961bcb6a8cce89292
Jonathan Daniel Bandoni <joni.bandoni@embrace.io>
2024-07-26 15:25:05 -0300

    create the empty project

---
+++ UalaCities/UalaCitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaCitiesApp.swift
+//  UalaCities
+//
+//  Created by Joni Bandoni on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct UalaCitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
